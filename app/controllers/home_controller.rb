01c7f83950ad4d6593c8f94dd04a2eff1d6fd8ec
Duke Dorje <dorje@Dorje.local>
2010-12-13 18:40:39 -0800

    Change login_box form stuff over to Rails-style form stuff. Messing with the routes.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if current_user
+      render :text => "logged in" and return
+    end
     @email = "phong@dreamcatcher.net"
+    @user = User.first
   end
 end
