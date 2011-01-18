766f11c5f96d16653c65043c302d6b0bc7d032df
Duke Dorje <dorje@Dorje.local>
2011-01-17 17:32:18 -0800

    Changed the tags migration.  Please run rake db:migrate:redo.

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(:version => 20110115040411) do
   create_table "authentications", :force => true do |t|
     t.string   "provider"
     t.string   "uid"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -96,8 +97,8 @@ ActiveRecord::Schema.define(:version => 20110115040411) do
 
   create_table "whos", :force => true do |t|
     t.string  "name"
-    t.string  "source"
-    t.integer "user",   :limit => 8
+    t.integer "user",      :limit => 8
+    t.string  "user_type"
   end
 
 end
