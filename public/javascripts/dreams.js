f8d8485ef8c9f29a5d676f8fe0c029beecfaf7be
Scott Hedstrom <scott@dreamcatcher.net>
2011-01-19 19:58:30 -0800

    javascript interactivity hooked up for most of images panel

--- public/javascripts/dreams.js
+++ public/javascripts/dreams.js
@@ -9,4 +9,52 @@ function setupEvents(){
   }, function(){
     $('#new_dream-images').slideUp();
   })
+  
+  resetImageButtons();
+}
+
+function resetImageButtons(){
+  // Click to remove Image
+  $('.imageRemoveButton').click(function(){
+    // Remove from list of used images
+    $(this).parent().parent().fadeOut();
+  })
+  
+  // Click to expand/contract image
+  $('.dreamImage').unbind();
+   $('.dreamImage').click(function(){
+     if($(this).hasClass('round-8')){
+       // Expand me
+       
+       // Close all other dream images that are open
+      $('.dreamImageContainer').each(function(){
+        if(!$(this).find('.dreamImage').hasClass('round-8')){
+          $(this).animate({width: '120px'}, 400, function(){
+            // Complete
+            $(this).find('.dreamImage').removeClass('round-8-left');
+            $(this).find('.dreamImage').addClass('round-8');
+          });
+          $(this).find('.dreamImage').animate({width: '120px'});
+        }
+      });
+      
+      $this = $(this);
+      $(this).parent().animate({width: '375px'}, 400, function(){
+        // Complete
+        $this.removeClass('round-8');
+        $this.addClass('round-8-left');
+      });
+      $(this).animate({width: '145px'});
+    
+     } else {
+      // Contract me
+      $this = $(this);
+      $(this).parent().animate({width: '120px'}, 400, function(){
+        // Complete
+        $this.removeClass('round-8-left');
+        $this.addClass('round-8');
+      });
+      $(this).animate({width: '120px'});
+     }
+   });
 }
