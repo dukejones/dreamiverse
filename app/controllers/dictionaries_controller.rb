fc4201343aac0883a9853dd3c80ec5c005d66bc1
Duke Dorje <dorje@Dorje.local>
2011-01-30 13:55:55 -0800

    Ability to edit an existing dictionary.  Also add the "attribution" into the dictionary editing form.

--- app/controllers/dictionaries_controller.rb
+++ app/controllers/dictionaries_controller.rb
@@ -7,8 +7,18 @@ class DictionariesController < ApplicationController
     @dict = Dictionary.new
   end
 
+  def edit
+    @dict = Dictionary.find(params[:id])
+  end
+  
   def create
     dict = Dictionary.create!(params[:dictionary])
+    redirect_to dictionary_words_path(dict)
+  end
+  
+  def update
+    dict = Dictionary.find(params[:id])
+    dict.update_attributes(params[:dictionary])
     redirect_to dictionary_words_path(dict)
   end
   
