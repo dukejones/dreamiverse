ade4fed4e470bb322b8707ac2498d22e20346297
Scott Hedstrom <scott@dreamcatcher.net>
2011-02-02 16:18:38 -0800

    speeding up expand state on friends hover

--- public/javascripts/context_menu.js
+++ public/javascripts/context_menu.js
@@ -8,13 +8,13 @@ function initContextMenu(){
   // Setup friends expander
   $('.friends').bind('mouseenter', function (e) {
     if(!friendsExpanded){
-      $(this).find('.expander').slideDown(500, function(){
+      $(this).find('.expander').slideDown(400, function(){
         friendsExpanded = true;
       });
     }
   })
   .bind('mouseleave', function (e) {
-    $(this).find('.expander').slideUp(500, function(){
+    $(this).find('.expander').slideUp(400, function(){
       friendsExpanded = false;
     });
   })
