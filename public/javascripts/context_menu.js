4d8f969c7ef25503a06a360230333b9c5d772c91
Scott Hedstrom <scott@dreamcatcher.net>
2011-02-02 16:05:43 -0800

    fixing glitchybuggyness!

--- public/javascripts/context_menu.js
+++ public/javascripts/context_menu.js
@@ -2,11 +2,20 @@ $(document).ready(function(){
   initContextMenu();
 })
 
+var friendsExpanded = false;
+
 function initContextMenu(){
   // Setup friends expander
-  $('.friends').hover(function(){
-    $(this).find('.expander').slideDown();
-  }, function(){
-    $(this).find('.expander').slideUp();
+  $('.friends').bind('mouseenter', function (e) {
+    if(!friendsExpanded){
+      $(this).find('.expander').slideDown(500, function(){
+        friendsExpanded = true;
+      });
+    }
+  })
+  .bind('mouseleave', function (e) {
+    $(this).find('.expander').slideUp(500, function(){
+      friendsExpanded = false;
+    });
   })
 }
