598e7a947c0c63f5f661a92e4bd3ae055af5dd2f
doktorj <doktorj@gmail.com>
2011-02-07 17:35:42 -0800

    added clean method to what model to assure alphanumeric only

--- app/models/what.rb
+++ app/models/what.rb
@@ -2,4 +2,10 @@ class What < ActiveRecord::Base
   has_many :tags, :as => :noun
 
   has_many :dreams, :through => :tags, :source => :entry, :source_type => 'Dream'
+  
+  before_create :clean
+  
+  def clean
+    self.name = self.name.gsub(/[^[:alnum:]]/, '') # replace non alpha numeric chars wiht nothing
+  end
 end
