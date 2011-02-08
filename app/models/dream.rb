547ab1ed35da354d9b667c91ee84fce34e1aed7f
doktorj <doktorj@gmail.com>
2011-02-07 17:22:12 -0800

    added callback and auto_generate tags method to dream model

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -6,8 +6,19 @@ class Dream < ActiveRecord::Base
   has_many :whos, :through => :tags, :source => :noun, :source_type => 'Who'
   has_many :wheres, :through => :tags, :source => :noun, :source_type => 'Where'
   
+  after_create :auto_generate_tags
+  
   def nouns
     whos + wheres + whats
     # tags.all(:include => :noun).map(&:noun) - seems to be slower.
   end
+  
+  def auto_generate_tags
+    tags = body.split(/\s+/) # make tags array by splitting body up on space chars
+    tags.each do |tag|
+      w = What.new(name: tag)
+      w.save
+    end
+  end
+  
 end
