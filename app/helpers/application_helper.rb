1653ef97c2e9591af49dbd76e703f5b7eb2f6f23
Duke Dorje <dorje@Dorje.local>
2011-02-07 21:04:01 -0800

    friend_icon_tag helper. friend_icon helper api change: pass relationship symbol rather than user subject+object. follow_action helper returns the string of the action to take. worked up the following partial.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,12 @@
 module ApplicationHelper
-  def friend_icon(subject, object, size)
-    case subject.relationship_with(object)
-    when :friend
+  def friend_icon_tag(relationship, size, html_options={})
+    html_options.merge!( size: "#{size}x#{size}", alt: relationship )
+    image_tag( "icons/#{friend_icon(relationship, size)}", html_options)
+  end
+  
+  def friend_icon(relationship, size)
+    case relationship
+    when :friends
       "friend-#{size}.png"
     when :following
       "friend-follower-#{size}.png"
@@ -12,6 +17,15 @@ module ApplicationHelper
     end
   end
 
+  def follow_action(relationship)
+    case relationship
+    when :friends, :following
+      "unfollow"
+    when :followed_by, :none
+      "follow"
+    end
+  end
+  
   def coffeescript_include_tag(*sources)
     javascript_include_tag(*(sources.map { |js| "compiled/#{js}" }))
   end
