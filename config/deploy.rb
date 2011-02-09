ed396915fe847c6df8368313304fe8dd468be1e5
Duke Dorje <dorje@Dorje.local>
2011-02-08 19:04:00 -0800

    Compile all coffeescripts upon deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_to, "/var/www/#{application}"
 after "deploy", "deploy:cleanup"
 after "deploy", "uploads:symlink"
 after "deploy", "deploy:migrate"
-
+after "deploy", "barista:brew"
 
 namespace :deploy do
   desc "Restarting mod_rails with restart.txt"
