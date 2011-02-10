34b832027805aecc735b60d7bcbe12c25cdf5b05
Ephraim Peniston <useruser@Ephraim-Peniston-2.local>
2011-02-09 21:45:13 -0800

    re-created starlight page with styles and new images

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,12 @@ class UsersController < ApplicationController
   def index
     @top_images = [
       "", # so its index starts at 1
-      "small-stellated-dodecahedron-DS1-128.png",
-      "great-icosahedron-DS2-128.png",
-      "small-ditrigonal-icosidodecahedron-DS3-128.png",
-      "great-icosahedron-DS4-128.png",
-      "small-ditrigonal-icosidodecahedron-DS5-128.png",
-      "great-icosahedron-DS6.png"
+      "dreamstars-1-128.png",
+      "dreamstars-2-128.png",
+      "dreamstars-3-128.png",
+      "dreamstars-4-128.png",
+      "dreamstars-5-128.png",
+      "dreamstars-6-128.png"
     ]
 
     @users = User.all
