9c7c116c99d92ae674f14d08e81027d62a431aea
Duke Dorje <dorje@Dorje.local>
2011-02-11 15:23:15 -0800

    Bunch of new columns in User: skype, phone, default_location, default_sharing_level, follow_authorization. Authentications now have a default_sharing option to share on their social site by default.  New indexes to speed querying of users and authentications. Added application_controller before_filter to set a session seed code so if a user signs up after browsing the site at all we'll have their seedcode saved to be either the username or the actual passed-in seedcode.  Updated user registration creation to add that feature.

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ActiveRecord::Base
   has_many :followings, :class_name => "Follow", :foreign_key => :following_id
   has_many :followers, :through => :followings, :source => :user
   
+  belongs_to :default_location, :class_name => "Where"
   
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
