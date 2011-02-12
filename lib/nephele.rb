2840d89d07b06b52339b52d330157ba3c519688a
doktorj <doktorj@gmail.com>
2011-02-11 17:38:00 -0800

    more auto-generated tag scoring improvements

--- lib/nephele.rb
+++ lib/nephele.rb
@@ -3,7 +3,7 @@ class Nephele
 =begin
   responsible for scoring, storing and generating single entry tag clouds, 
   multi-entry tag clouds and tag context clouds (clouds attached to tags)
-  doktorj@dreamcatcher.net
+  started feb/2011 - doktorj@dreamcatcher.net
 =end
   
   # invoke processes for scoring custom tags and 
@@ -16,12 +16,12 @@ class Nephele
   def auto_generate_single_entry_tags(dream)
     tag_string = dream.body << ' ' + dream.title #concat body/title
     tags = tag_string.split(/\s+/)
-    return score_freq(tags)
+    return score_freq(dream,tags)
   end
     
   # takes in a normal array of keywords and returns hash of noun (what) ids
   # and their associated score/freq
-  def score_freq(tags,num_scores = 16)
+  def score_freq(dream,tags,num_scores = 16)
     freqs = {} #init hash
     black_list_words = BlackListWord.find(:all).map{|w| w.what.name }
     
@@ -33,18 +33,26 @@ class Nephele
         new_count = 0
         tag_id = get_what_id(tag) 
         if !tag_id.nil?          
-          if freqs[tag].nil? 
-            freqs[tag] = 1 # first instance of this tag_id
+          if freqs[tag_id].nil? 
+            freqs[tag_id] = 1 # first instance of this tag_id
           else  
-            freqs[tag]+=1  # increment score for this tag id
+            freqs[tag_id]+=1  # increment score for this tag id
           end                 
         end
       end
     end
     
+    # sort results and keep the top (num_scores)
     freqs = freqs.sort_by { |key,value| value }.reverse #sort by value, reversed   
-    freqs = freqs.first(num_scores) # grab the top 16 elements
+    freqs = freqs.first(num_scores) # grab the top num_scores elements
     freqs = Hash[*freqs.flatten] #convert back into a hash
+    
+    # clear_scores(dream.id,nephele_id?) - for edit/update mode only
+    # save the scores
+    freqs.each do |tag_id,score|
+      save_score(dream,nephele_id?,tag_id,'What',score)
+    end  
+              
     return freqs
   end
 
@@ -55,13 +63,31 @@ class Nephele
     return w.id 
   end
   
-  # downcase & strip non alpha numeric chars at begin/end of string
+  # downcase & strip non alpha numeric chars at begin/end of tag
   def self.prep_tag(tag)
     return tag.downcase.gsub(/^\W+|\W+$/, '') 
   end  
   
   def nephele_id?
-    return User.find_by_username('nephele')
+    u = User.find_by_username('nephele')
+    return u.id 
   end
+  
+  # insert a tags record
+  def save_score(dream,user_id,noun_id,noun_type,score)
+  
+    Tag.create( :entry_id   => dream.id, 
+                :entry_type => 'Dream',
+                :user_id    => user_id,
+                :noun_id    => noun_id,
+                :noun_type  => 'What',
+                :score      => score)
+  end
+  
+  def clear_scores(entry_id,user_id)
+    if entry_id > 0 && user_id > 0 
+      Tag.delete_all(:entry_id => entry_id, :user_id => user_id)
+    end  
+  end  
     
 end
