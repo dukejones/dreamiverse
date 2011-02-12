99a9090d58712cc469e8a9d96954be830cc8f9d8
doktorj <doktorj@gmail.com>
2011-02-11 22:16:27 -0800

    re-added what.rb + rebuilt schema.rb (forgot add . before last commit)

---
+++ app/models/what.rb
@@ -0,0 +1,17 @@
+class What < ActiveRecord::Base
+  has_many :tags, :as => :noun
+  has_many :dreams, :through => :tags, :source => :entry, :source_type => 'Dream'
+  has_many :black_list_words
+  
+  validates :name,
+            :presence => true,
+            :uniqueness => true,
+            :length => { :minimum => 3, :maximum => 40 }
+ 
+  before_create :clean
+  
+  def clean
+    self.name = Nephele.prep_tag(self.name)
+  end
+  
+end
