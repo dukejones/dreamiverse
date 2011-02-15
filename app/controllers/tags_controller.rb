7502ab89612766a808e92ae7fa60403d5e3b0dad
Duke Dorje <dorje@Dorje.local>
2011-02-14 22:41:51 -0800

    Tag creation & destroy via ajax calls.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,14 +1,22 @@
 class TagsController < ApplicationController
   def create
     @entry = Entry.find params[:entry_id]
-    @entry.whats.create!(params[:tag_name]) # :created_by => current_user
+
+    what = What.find_or_create_by_name(params[:what_name])
+    # @entry.whats.create!(params[:what_name]) # :created_by => current_user
+    Tag.create!(entry: @entry, noun: what)
+    
     render :json => "created"
   rescue => e
     render :json => e.message, :status => :unprocessable_entity
   end
   
   def destroy
-    
+    what = What.find_by_name(params[:what_name])
+    Tag.destroy!(entry_id: params[:entry_id], noun: what)
+    render :json => "destroyed"
+  rescue => e
+    render :json => e.message, :status => :unprocessable_entity
   end
 
 end
