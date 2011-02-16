13b95bc5c926d42d761d6641f15865d81def7825
Duke Dorje <dorje@Dorje.local>
2011-02-15 21:32:58 -0800

    Not bedsheet, image!

--- app/models/view_preference.rb
+++ app/models/view_preference.rb
@@ -4,6 +4,6 @@ class ViewPreference < ActiveRecord::Base
   belongs_to :viewable, :polymorphic => true
   
   def clone!
-    self.class.create(:theme => theme, :bedsheet => bedsheet)
+    self.class.create(:theme => theme, :image => image)
   end
 end
