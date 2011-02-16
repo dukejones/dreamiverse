809007f4972f65b425e80bea1b2d12a5b80984bd
Duke Dorje <dorje@Dorje.local>
2011-02-15 17:36:39 -0800

    Refactor the class_size calculations into the Tag class, and using the View.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,28 @@ class Tag < ActiveRecord::Base
   belongs_to :entry
   belongs_to :noun, :polymorphic => true
   validates_uniqueness_of :entry_id, :scope => [:noun_id, :noun_type], :message => "Entry already has this tag."
+
+  attr_accessor :class_size
+
+  # render html tag cloud with css font sizes/colors
+  def self.with_class_sizes_for(tags)
+    min_score = tags.map(&:score).min
+    max_score = tags.map(&:score).max
+    tags.shuffle
+    tags.each do |tag|
+      tag.class_size = quantize(tag.score, min_score, max_score)
+    end
+    
+    tags
+  end
+  
+  
+private
+  # returns a number: 1-8
+  def self.quantize(score, min_score, max_score)
+    score_range = (max_score == min_score) ? 1 : (max_score - min_score)
+    scaling_factor = (8 - 1) / score_range
+    (((score-min_score) * scaling_factor) + 1).to_i
+  end
+
 end
--- lib/nephele.rb
+++ lib/nephele.rb
@@ -17,57 +17,65 @@ class Nephele
 
 
   # render html tag cloud with css font sizes/colors
-  def self.render_single_entry_tag_cloud(entry_id,user_id)
-   
-    puts "test: running render_single_entry_tag_cloud( "+ entry_id.to_s + "," + user_id.to_s + ")" 
-    tag_scores = Hash.new
-    tags = Tag.where(:entry_id => entry_id,:user_id => [user_id,2]).limit(16)
-    tags.map{ |tag| tag_scores[tag.noun_id] = tag.score }
-    
-    # some test tag_scores
-    #tag_scores = { 544=>11, 538=>8, 517=>6, 524=>4, 548=>32, 592=>1 }
-    #tag_scores = { 544=>11, 538=>10, 517=>10, 524=>9, 548=>9, 592=>9, 516=>8, 551=>7, 537=>7, 566=>7, 549=>6, 523=>6, 589=>6, 581=>6, 564=>5, 547=>5 }
-    
-    maxSize = 8 # max font size class
-    minSize = 1 # min font size class
-    
-    # largest and smallest hash values
-    maxQty = tag_scores.values.max
-    minQty = tag_scores.values.min
-
-    # find the range of values
-    spread = maxQty - minQty
-    spread = 1 if spread == 0 # we want to avoid divide by zero errors
- 
-    # set the class_size increment
-    step = (maxSize.to_f - minSize.to_f)
-    step = step / spread.to_f
-
-    #puts 'maxSize: ' + maxSize.to_s + ' minSize: ' + minSize.to_s + ' maxQty: ' + maxQty.to_s + ' minQty: ' + minQty.to_s;
-    #puts ' spread: ' + spread.to_s#  + ' step: ' + step.to_f
-
-    tag_cloud = []
-
-    tag_scores.each do |what_id,score|
-      what = What.find(what_id)
-      class_size = minSize + ((score - minQty) * step).to_i # the to_i leaves us with a whole #
-      #puts 'what_id: ' + what_id.to_s + ' name: ' + what.name +  ' score: ' + score.to_s + ' class_size: ' + class_size.to_s  
-      tag_cloud.push('<div class="TC s-' + class_size.to_s + ' water">' + what.name.to_s + '</div>')
-    end
-
-    tag_cloud = tag_cloud.shuffle
-
-    #puts 'tag_cloud: ' + tag_cloud.to_s
-    
-  
-    # turn it into a string for the view
-    tag_cloud_string = '' # init
-    tag_cloud.each do |tag|
-     tag_cloud_string = tag_cloud_string + tag
-    end
-    
-    return tag_cloud_string
-  end
+  # def self.render_single_entry_tag_cloud(entry)
+  # 
+  #   # puts "test: running render_single_entry_tag_cloud( "+ entry_id.to_s + "," + user_id.to_s + ")" 
+  #   tag_scores = {}
+  #   # tags = Tag.where(:entry_id => entry_id,:user_id => [user_id,2]).limit(16)
+  #   tag = entry.tags.limit(16)
+  #   tags.map{ |tag| tag_scores[tag.noun_id] = tag.score }
+  #   
+  # 
+  #   
+  # 
+  #   # some test tag_scores
+  #   # tag_scores = { 544=>11, 538=>8, 517=>6, 524=>4, 548=>32, 592=>1 }
+  #   # tag_scores = { 544=>11, 538=>10, 517=>10, 524=>9, 548=>9, 592=>9, 516=>8, 551=>7, 537=>7, 566=>7, 549=>6, 523=>6, 589=>6, 581=>6, 564=>5, 547=>5 }
+  # 
+  #   maxSize = 8 # max font size class
+  #   minSize = 1 # min font size class
+  #   
+  #   # largest and smallest hash values
+  #   maxQty = tag_scores.values.max
+  #   minQty = tag_scores.values.min
+  # 
+  # 
+  #   # find the range of values
+  #   spread = maxQty - minQty
+  #   spread = 1 if spread == 0 # we want to avoid divide by zero errors
+  #  
+  #   # set the class_size increment
+  #   step = (maxSize.to_f - minSize.to_f)
+  #   step = step / spread.to_f
+  # 
+  #   #puts 'maxSize: ' + maxSize.to_s + ' minSize: ' + minSize.to_s + ' maxQty: ' + maxQty.to_s + ' minQty: ' + minQty.to_s;
+  #   #puts ' spread: ' + spread.to_s#  + ' step: ' + step.to_f
+  # 
+  #   tag_cloud = []
+  # 
+  # 
+  # 
+  #   tag_scores.each do |what_id,score|
+  #     what = What.find(what_id)
+  #     
+  #     class_size = minSize + ((score - minQty) * step).to_i # the to_i leaves us with a whole #
+  #     #puts 'what_id: ' + what_id.to_s + ' name: ' + what.name +  ' score: ' + score.to_s + ' class_size: ' + class_size.to_s  
+  #     tag_cloud.push('<div class="TC s-' + class_size.to_s + ' water">' + what.name.to_s + '</div>')
+  #   end
+  # 
+  #   tag_cloud = tag_cloud.shuffle
+  # 
+  #   #puts 'tag_cloud: ' + tag_cloud.to_s
+  #   
+  # 
+  #   # turn it into a string for the view
+  #   tag_cloud_string = '' # init
+  #   tag_cloud.each do |tag|
+  #    tag_cloud_string = tag_cloud_string + tag
+  #   end
+  #   
+  #   return tag_cloud_string
+  # end
 
   def auto_generate_single_entry_tags(dream)
     tag_string = dream.body << ' ' + dream.title #concat body/title
@@ -130,8 +138,9 @@ private ###################
   
   
   def nephele_id?
-    u = User.find_by_username('nephele')
-    return u.id 
+    128
+    # u = User.find_by_username('nephele')
+    # return u.id 
   end
   
   # insert a tags record
