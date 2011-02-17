5ac6c752b4f3edee7eb1a1f903c498478206b359
Scott Hedstrom <scott@dreamcatcher.net>
2011-02-17 15:47:03 -0800

    fixing image uploader to dream

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,2 +1,15 @@
 module EntriesHelper
+  def gallery_list_item(image)
+    small_image_url = image.url(:thumb, 120)
+    large_image_url = image.url(:medium)
+    raw(%{
+      <li style="background: #ababab url(#{small_image_url}) no-repeat">
+        <a href="#{large_image_url}"></a>
+      </li>
+    })
+  end
+  
+  def dream_header_url(image)
+    image._?.url(:header)
+  end
 end
