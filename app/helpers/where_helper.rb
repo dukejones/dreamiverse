bd53e4e5fb2ad8094c9d77ae63ea337e43c27814
Scott Hedstrom <scott@dreamcatcher.net>
2011-02-25 12:36:53 -0800

    updating where helper

--- app/helpers/where_helper.rb
+++ app/helpers/where_helper.rb
@@ -1,4 +1,6 @@
-def get_city_prov(id)
-  w = Where.find_by_id(1)
-  "#{w.city}, #{w.province}"
+module WhereHelper
+  def get_city_prov(id)
+    w = Where.find_by_id(id)
+    "#{w.city}, #{w.province}"
+  end
 end
