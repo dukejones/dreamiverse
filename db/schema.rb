4825d5c66690866be9fca52cf0cba15d4463735b
doktorj <doktorj@gmail.com>
2011-02-26 15:42:01 -0800

    misc tweaks/cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -112,7 +112,7 @@ ActiveRecord::Schema.define(:version => 20110223163252) do
     t.text     "notes"
     t.integer  "uploaded_by_id"
     t.string   "geotag"
-    t.text     "tags",                                               :null => false
+    t.text     "tags"
     t.boolean  "public"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -150,10 +150,9 @@ ActiveRecord::Schema.define(:version => 20110223163252) do
     t.string  "entry_type", :default => "Dream"
     t.integer "noun_id"
     t.string  "noun_type",  :default => "What"
-    t.integer "user_id"
-    t.integer "position",   :default => 0
-    t.string  "kind",       :default => "custom"
     t.integer "intensity"
+    t.integer "position",   :default => 0
+    t.string  "kind",       :default => "custom", :null => false
   end
 
   create_table "users", :force => true do |t|
