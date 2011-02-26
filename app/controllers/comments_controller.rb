66a6c9b57a894b2e634dce8ba9e19a61bdad51bb
Geoff <geoff@dreamcatcher.net>
2011-02-25 22:39:47 -0800

    comments now delete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,18 @@
 class CommentsController < ApplicationController
   def create
     raise "Empty comment!" if params[:comment][:body].blank?
+    @entry = Entry.find params[:entry_id]
     created_comment = Comment.create!(params[:comment].merge(entry_id: params[:entry_id]))
     respond_to do |format|
       format.html { render :partial => 'entries/comment', :object => created_comment }
       format.json { render :json => { :comment => created_comment } }
     end
+  end
+  
+  def destroy
+    comment = Comment.where(id: params[:id], entry_id: params[:entry_id]).first
+    comment.destroy
+    
+    render :json => { type: 'ok', message: "Comment #{comment.id} destroyed."}
   end
 end
