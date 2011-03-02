c37b6f5b03d095f74a2465482599f4e8e08f38b7
Scott Hedstrom <scott@dreamcatcher.net>
2011-03-02 14:50:00 -0800

    adding youtube class to linkContainer

--- public/javascripts/dreams.js
+++ public/javascripts/dreams.js
@@ -412,7 +412,7 @@ function showYoutubeData(newText){
     dataType: 'jsonp',
     success: function(data) {
       console.log(data)
-      var newElement = '<div class="linkContainer"><div class="description">' + data.feed.entry[0].content.$t + '</div><div class="thumb"><img width="120" height="90" src="' + data.feed.entry[0].media$group.media$thumbnail[0].url + '" /></div><div class="title"><input class="linkTitleValue" value="' + data.feed.entry[0].title.$t + '" name="entry[links_attributes][][title]" /></div><div class="url"><input value="' + newText + '" class="linkTitleValue" name="entry[links_attributes][][url]" style="width: 320px;"></div><div class="removeicon">X</div><div class="icon"><img src="http://www.google.com/s2/favicons?domain_url=' + newText + '" /></div></div>';
+      var newElement = '<div class="linkContainer youtube"><div class="description">' + data.feed.entry[0].content.$t + '</div><div class="thumb"><img width="120" height="90" src="' + data.feed.entry[0].media$group.media$thumbnail[0].url + '" /></div><div class="title"><input class="linkTitleValue" value="' + data.feed.entry[0].title.$t + '" name="entry[links_attributes][][title]" /></div><div class="url"><input value="' + newText + '" class="linkTitleValue" name="entry[links_attributes][][url]" style="width: 320px;"></div><div class="removeicon">X</div><div class="icon"><img src="http://www.google.com/s2/favicons?domain_url=' + newText + '" /></div></div>';
       $('#linkHolder').append(newElement);
       $('.linkContainer').fadeIn();
     }
