bbe001483aa373f7df3756db9fcfa4ca80cb61d3
Duke Dorje <dorje@Dorje.local>
2011-03-05 02:28:24 -0800

    OK, maybe let's put it in environment.rb.

--- config/environment.rb
+++ config/environment.rb
@@ -21,3 +21,14 @@ ActiveRecord::Base.include_root_in_json = false
 # Recaptcha stuff
 ENV['RECAPTCHA_PUBLIC_KEY']  = '6LcfLMISAAAAAFSBbUqJIT18uqIQdlqXnb1feFN5'
 ENV['RECAPTCHA_PRIVATE_KEY'] = '6LcfLMISAAAAANIq7MjfmzqNWZH_KS52xFfoXgui'
+
+
+gmail_smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "dreamcatcher.net",
+  user_name: "mailer@dreamcatcher.net",
+  password: "G9%Ln8(qtmZ3N3FZ5aTr",
+  authentication: "plain",
+  enable_starttls_auto: true
+}
