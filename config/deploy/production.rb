8faddb06e66cc88701011bb0a66306d001e6f6ec
Duke Dorje <dorje@Dorje.local>
2011-03-09 12:44:24 -0800

    Theta use shared image dir.  Production use imagebank.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,4 +7,11 @@ set :deploy_to, "/var/www/#{application}"
 
 server "dreamcatcher.net", :web, :app, :db, :primary => true
 
+namespace :uploads do
+  desc "Symlink the uploads directory to the shared uploads directory."
+  task :symlink do
+    run "rm -rf #{current_path}/public/images/uploads"
+    run "ln -fs /mnt/imagebank #{current_path}/public/images/uploads"
+  end
+end
 
--- config/deploy/theta.rb
+++ config/deploy/theta.rb
@@ -6,3 +6,16 @@ set :deploy_to, "/var/www/#{application}"
 
 server "dev.dreamcatcher.net", :web, :app, :db, :primary => true
 
+before 'uploads:symlink', 'uploads:create_shared'
+namespace :uploads do
+  desc "Symlink the uploads directory to the shared uploads directory."
+  task :symlink do
+    run "rm -rf #{current_path}/public/images/uploads"
+    run "ln -fs #{shared_path}/images/uploads #{current_path}/public/images/"
+  end
+  
+  desc "Create the shared image uploads directory if it doesn't exist, and set the correct permissions."
+  task :create_shared do
+    run "mkdir -p #{shared_path}/images/uploads/originals; chmod -R 777 #{shared_path}/images/uploads"
+  end
+end
