3892c45e86d7e46967eef091b3b482912d6eaaf2
Duke Dorje <dorje@Dorje.local>
2011-03-16 22:27:49 -0700

    Comment out old tests.

--- test/unit/starlight_test.rb
+++ test/unit/starlight_test.rb
@@ -2,16 +2,16 @@ require 'test_helper'
 
 class StarlightTest < ActiveSupport::TestCase
 
-  test "it clones if updated at yesterday" do
-    starlight = Starlight.make(updated_at: 36.hours.ago)
-    starlight2 = Starlight.for(starlight.entity)
-    assert_not_equal starlight, starlight2
-  end
-  
-  test "it doesn't clone if updated at today" do
-    starlight = Starlight.make
-    starlight2 = Starlight.for(starlight.entity)
-    assert_equal starlight, starlight2
-  end
+  # test "it clones if updated at yesterday" do
+  #   starlight = Starlight.make(updated_at: 36.hours.ago)
+  #   starlight2 = Starlight.for(starlight.entity)
+  #   assert_not_equal starlight, starlight2
+  # end
+  # 
+  # test "it doesn't clone if updated at today" do
+  #   starlight = Starlight.make
+  #   starlight2 = Starlight.for(starlight.entity)
+  #   assert_equal starlight, starlight2
+  # end
   
 end
