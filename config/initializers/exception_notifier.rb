d40c9591417fc2de5e20aa5db812408ca2072bc8
Duke Dorje <dorje@Dorje.local>
2011-03-31 14:43:39 -0700

    Add app name & require the exception_notifier gem.

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,4 +1,5 @@
-Whatever::Application.config.middleware.use ExceptionNotifier,
+require 'exception_notifier'
+Dreamcatcher::Application.config.middleware.use ExceptionNotifier,
   :email_prefix => "[ERROR] ",
   :sender_address => %{"dreamcatcher.net" <mailer@dreamcatcher.net>},
   :exception_recipients => %w{support@dreamcatcher.net}
