de0bceecb8cdd083b218d623ecbe4e0b94b168bd
Duke Dorje <dorje@Dorje.local>
2011-03-31 15:38:20 -0700

    Only use exception notification if it's not development.

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,6 +1,7 @@
-require 'exception_notifier'
-Dreamcatcher::Application.config.middleware.use ExceptionNotifier,
-  :email_prefix => "[ERROR] ",
-  :sender_address => %{"dreamcatcher.net" <mailer@dreamcatcher.net>},
-  :exception_recipients => %w{support@dreamcatcher.net}
-  
+unless Rails.env == 'development'
+  require 'exception_notifier'
+  Dreamcatcher::Application.config.middleware.use ExceptionNotifier,
+    :email_prefix => "[ERROR] ",
+    :sender_address => %{"dreamcatcher.net" <mailer@dreamcatcher.net>},
+    :exception_recipients => %w{support@dreamcatcher.net}
+end
