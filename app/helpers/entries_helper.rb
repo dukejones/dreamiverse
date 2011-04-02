897ced7fda2a621f5dd5faa99efc3545752c6d3a
Duke Dorje <dorje@Dorje.local>
2011-04-02 15:56:13 -0700

    Let's just use the new helper.

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -6,20 +6,22 @@ module EntriesHelper
   end
   
   def dreamfield_header_image
-    img_url = case @type_filter
-    when "dream"
-      "/images/icons/dream-32-hover.png"
-    when "vision"
-      "/images/icons/vision-32-hover.png"
-    when "experience"
-      "/images/icons/experience-32-hover.png"
-    when "article"
-      "/images/icons/article-32-hover.png"
-    else
-      "/images/icons/dream-32-hover.png"
-    end
+    return entry_type_icon(@type_filter, 32, 'hover')
     
-    raw( img_url )
+    # img_url = case @type_filter
+    # when "dream"
+    #   "/images/icons/dream-32-hover.png"
+    # when "vision"
+    #   "/images/icons/vision-32-hover.png"
+    # when "experience"
+    #   "/images/icons/experience-32-hover.png"
+    # when "article"
+    #   "/images/icons/article-32-hover.png"
+    # else
+    #   "/images/icons/dream-32-hover.png"
+    # end
+    # 
+    # raw( img_url )
   end
 
   def gallery_list_item(image)
