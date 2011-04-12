0e744490ea19ad7c72998df890af4905f9d38536
Duke Dorje <dorje@Dorje.local>
2011-04-11 17:25:41 -0700

    Make exception_notifier include a user section.  Make that section display the user information.

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,6 +1,7 @@
 unless Rails.env == 'development'
   require 'exception_notifier'
   Dreamcatcher::Application.config.middleware.use ExceptionNotifier,
+    :sections => %w(user request session environment backtrace),
     :email_prefix => "[ERROR] ",
     :sender_address => %{"dreamcatcher.net" <mailer@dreamcatcher.net>},
     :exception_recipients => %w{support@dreamcatcher.net}
