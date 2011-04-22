465bb5647719d3f65e8d96e240a083998cf25697
Duke Dorje <dorje@Dorje.local>
2011-04-22 00:08:11 -0700

    Default the new_comment_count in the db, simplify the controller code.

--- db/migrate/20110420050326_add_new_comment_count_to_entries.rb
+++ db/migrate/20110420050326_add_new_comment_count_to_entries.rb
@@ -1,6 +1,6 @@
 class AddNewCommentCountToEntries < ActiveRecord::Migration
   def self.up
-    add_column :entries, :new_comment_count, :integer
+    add_column :entries, :new_comment_count, :integer, :default => 0
   end
 
   def self.down
