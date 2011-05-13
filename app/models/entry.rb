71d39a55feec4d6d21ec25d12616cb4fb61a6cca
Duke Dorje <dorje@Dorje.local>
2011-05-12 18:03:41 -0700

    Was showing strange numbers of entries per page on the dreamfield.  So, do the selection with SQL, and do the ruby as a backup.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -173,6 +173,11 @@ class Entry < ActiveRecord::Base
     entry_scope = entry_scope.offset(page_size * (page - 1))
     
     if viewer
+      entry_scope = entry_scope.where(:sharing_level ^ self::Sharing[:private]) unless viewer == viewed
+      entry_scope = entry_scope.where(:sharing_level ^ self::Sharing[:anonymous]) unless viewer == viewed
+      entry_scope = entry_scope.where(:sharing_level ^ self::Sharing[:followers]) unless viewer.following?(viewed)
+      entry_scope = entry_scope.where(:sharing_level ^ self::Sharing[:friends]) unless viewer.friends_with?(viewed)
+      # TODO: Put a log warning here if it eliminates any entries.  So we can get rid of this line.
       entries = entry_scope.select {|e| viewer.can_access?(e) }
     else
       entries = entry_scope.where(sharing_level: self::Sharing[:everyone])
