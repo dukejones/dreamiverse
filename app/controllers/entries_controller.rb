7783c5e728eebf2c2241c1afbb0dc167e6305490
Duke Dorje <dorje@Dorje.local>
2011-05-15 11:23:36 -0700

    For the stream entry's entry#show forward & back buttons, save the filters in the session.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -127,6 +127,7 @@ class EntriesController < ApplicationController
   def stream
     @user = current_user
     @filters = @user.update_stream_filter(params[:filters])
+    session[:filters] = @filters
     @entries = entry_list(:stream, @filters)
     
     if request.xhr?
