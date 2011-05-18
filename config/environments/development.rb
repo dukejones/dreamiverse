ce555e8977f4a9046ef39ac9de23aebf61adcbef
Duke Dorje <dorje@Dorje.local>
2011-05-17 19:34:33 -0700

    Make migration to set defaults for user default settings. Turn on caching for dev.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,9 +16,9 @@ Dreamcatcher::Application.configure do
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_view.debug_rjs             = true
-  config.action_controller.perform_caching = false
-  # config.action_controller.perform_caching = true
-  # config.cache_store = :mem_cache_store
+  # config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
+  config.cache_store = :mem_cache_store
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
