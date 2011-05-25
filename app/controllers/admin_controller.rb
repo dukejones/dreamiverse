97a7947b51ae1b2cd0548c213bc3cc5fe9469aac
doktor J <doktorj@gmail.com>
2011-05-25 12:47:23 -0700

    removed extra totalPages ceil() count in ruby, and renamed _users partial to _user

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -8,18 +8,13 @@ class AdminController < ApplicationController
     page = params[:page].to_i
     page = 1 if page <= 0   
     
-    # debugger
-    # 1
-
     @users = User.where(:username ^ 'feh')
     @users = @users.limit(page_size).offset(page_size * (page - 1))
     @page_size = page_size
-    @total_pages = (@users.count / page_size)
-    @total_pages = @total_pages.ceil + 1
     
     if request.xhr?
       users_html = ""
-      @users.each { |user| users_html += render_to_string(:partial => 'users', :locals => {:user => user}) }
+      @users.each { |user| users_html += render_to_string(:partial => 'user', :locals => {:user => user}) }
       render :json => {type: 'ok', html: users_html}
     end    
        
