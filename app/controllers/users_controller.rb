55eff8a3f789fa81c39b5d6313c81e761eafd188
Geoff <geoff@dreamcatcher.net>
2011-06-27 17:14:13 -0700

    moved context panel and meta menu OUT from the layouts INTO common

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -134,7 +134,7 @@ class UsersController < ApplicationController
     
     
     if @user
-      render(:partial => "layouts/context_panel", :locals => {:user => @user, :book => @book})
+      render(:partial => "common/context_panel", :locals => {:user => @user, :book => @book})
     else
       render :text => "Could not find this user.", :status => 403
     end
