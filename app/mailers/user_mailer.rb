60c84b823d0033a3cb5e5c3006cb7652e02a4e94
doktor J <doktorj@gmail.com>
2011-06-29 16:18:27 -0700

    added more mailer methods/templates for long username update emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -21,8 +21,22 @@ class UserMailer < ActionMailer::Base
     @reset_url = reset_password_url(user.username, user.password_reset_code)
     mail(to: "#{user.name} <#{user.email}>", subject: "Password reset")
   end
-
+  
+  # Give a 30 day warning that their username is too long
   def warn_long_username_email(user)
+    @user = user
+    mail(to: "#{user.name} <#{user.email}>", subject: "Dreamcatcher.net account update")
+  end  
+  
+  # Inform user that their new username request was received and applied 
+  def long_username_request_updated(user)
+    @user = user
+    mail(to: "#{user.name} <#{user.email}>", subject: "Dreamcatcher.net account update")
+  end
+  
+  # Inform user that we did not hear from them regarding their new username within 30 days
+  # of our email warning so their new username has been set to: 
+  def long_username_updated(user)
     @user = user
     mail(to: "#{user.name} <#{user.email}>", subject: "Dreamcatcher.net account update")
   end  
