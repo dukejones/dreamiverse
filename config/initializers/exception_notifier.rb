91358498b191e56f520b066997a7f74c2f1ee841
Duke Dorje <duke.dorje@gmail.com>
2015-09-17 00:34:09 -0700

    Fix more exception notifier initializer stuff

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,10 +1,11 @@
 # ExceptionNotifier::Notifier.prepend_view_path File.join(Rails.root, 'app/views')
 
 if !Rails.env.development? && !Rails.env.test?
-  require 'exception_notifier'
-  Rails.application.config.middleware.use ExceptionNotifier,
+  Rails.application.config.middleware.use ExceptionNotification::Rack,
     :sections => %w(user request session environment backtrace),
-    :email_prefix => "[ERROR] ",
-    :sender_address => %{"dreamcatcher.net" <mailer@dreamcatcher.net>},
-    :exception_recipients => %w{support@dreamcatcher.net}
+    :email => {
+      :email_prefix => "[ERROR] ",
+      :sender_address => %{"dreamcatcher.net" <mailer@dreamcatcher.net>},
+      :exception_recipients => %w{support@dreamcatcher.net}
+    }
 end
