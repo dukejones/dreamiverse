136151fb53da3b235fd2a6bb6602ee3cd0743ce1
Duke Dorje <duke.dorje@gmail.com>
2015-09-17 01:00:13 -0700

    Fix migration

--- db/migrate/20110531231745_create_books.rb
+++ db/migrate/20110531231745_create_books.rb
@@ -1,6 +1,6 @@
 class CreateBooks < ActiveRecord::Migration
   def self.up
-    drop_table :books # somehow this table is in the prod db.  :-(
+    # drop_table :books # somehow this table is in the prod db.  :-(
     create_table :books do |t|
       t.string :title
       t.integer :user_id
@@ -8,7 +8,7 @@ class CreateBooks < ActiveRecord::Migration
       t.string :color
       t.integer :sharing_level
       t.integer :commenting_level
-      
+
       t.timestamps
     end
   end
