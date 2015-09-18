c584214d5d5e496161592c0cda354db7f53b09e4
Duke Dorje <duke.dorje@gmail.com>
2015-09-18 00:35:42 -0700

    Fix books in context panel, and old javascript include in friends.

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :keep_releases, 5
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/imagebank')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/imagebank', 'public/imagebank-cache')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
